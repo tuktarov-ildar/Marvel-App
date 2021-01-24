b9d503b262d3d6df99fcce5ddc7a65f9840b64ae
Ildar Tuktarov <>
2021-01-24 16:22:22 +0400

    Initial changes are set up

---
+++ Marvel App/Modules/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel App
+//
+//  Created by Ильдар Туктаров on 23.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
